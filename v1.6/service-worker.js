fbdc470e7a8d5e0bcafd1862a50d2f03f0bed4ea
Sarwan Yadav <rangersarwan@gmail.com>
2023-04-14 12:54:27 +0530

    ...

--- v1.6/service-worker.js
+++ v1.6/service-worker.js
@@ -1,30 +1,50 @@
-const cacheName = 'my-app-cache';
-const filesToCache = [
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
   '/',
   '/index.html',
   '/preloader.css',
-  'img/skill.png',
-  'img/6139.jpg',
-  'files/v1.2.html',
-  'files/v1.2.6.8.html',
-  'files/v1.6.html',
-  'files/v1.8.html',
-  'screenshots/1',
-  'screenshots/2',
-  'screenshots/3',
-  'screenshots/4',
-  'screenshots/5',
-  'screenshots/6',
-  'https://mdbcdn.b-cdn.net/img/new/slides/003.webp' // Add this line
+  '/img/skill.png',
+  '/img/6139.jpg',
+  '/files/v1.2.html',
+  '/files/v1.2.6.8.html',
+  '/files/v1.6.html',
+  '/files/v1.8.html',
+  '/screenshots/1',
+  '/screenshots/2',
+  '/screenshots/3',
+  '/screenshots/4',
+  '/screenshots/5',
+  '/screenshots/6',
+  'https://mdbcdn.b-cdn.net/img/new/slides/003.webp'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName)
-      .then(function(cache) {
-        return cache.addAll(filesToCache);
-      })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
